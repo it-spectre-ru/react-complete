d0031c300ab7b868c607ddaec451379c6a6c0fe8
tsvetkovpro <285019@gmail.com>
2016-12-15 07:45:07 +0500

    first react component

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,15 @@
+var Greeter = React.createClass({
+	render: function () {
+		return (
+			<div>
+				<h1>hello...</h1>
+				<p>this is from a component...</p>
+			</div>
+		);
+	}
+});
+
 ReactDOM.render(
-	<h1>hello</h1>,
+	<Greeter/>,
 	document.getElementById('app')
 );
