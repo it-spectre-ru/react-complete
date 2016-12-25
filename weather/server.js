a2ca74acf0ea8dcc7d3d4507b10a40ebb256f09f
tsvetkovpro <285019@gmail.com>
2016-12-25 11:55:24 +0500

    deploying app on heroku

--- weather/server.js
+++ weather/server.js
@@ -1,10 +1,19 @@
-var express = require( 'express' );
+var express = require('express');
 
-// create our app
+// Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use( express.static( 'public' ) );
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
-app.listen( 3000, function () {
-	console.log( 'server upp 3000 port' );
-} );
+app.use(express.static('public'));
+
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
+});
