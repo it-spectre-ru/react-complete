d35b28adc1cbc1efb5532fe06f9daaee73a1598e
tsvetkovpro <285019@gmail.com>
2017-01-03 12:13:37 +0500

    component lifecycle
    update status to stop when count reaches zero

--- timer/app/components/Countdown.jsx
+++ timer/app/components/Countdown.jsx
@@ -18,13 +18,17 @@ var Countdown = React.createClass ({
 					this.startTimer ();
 					break;
 				case 'stopped':
-					this.setState({count: 0});
+					this.setState ({count: 0});
 				case 'paused':
-					clearInterval(this.timer);
+					clearInterval (this.timer);
 					this.timer = undefined;
 					break;
 			}
 		}
+	},
+	componentWillUnmount: function () {
+		clearInterval (this.timer);
+		this.timer = undefined;
 	},
 	startTimer: function () {
 		this.timer = setInterval (() => {
@@ -33,7 +37,9 @@ var Countdown = React.createClass ({
 				count: newCount >= 0 ? newCount : 0
 			});
 
-
+			if (newCount === 0) {
+				this.setState({countdownStatus: 'stopped'});
+			}
 		}, 1000);
 	},
 	handleSetCountdown: function (seconds) {
