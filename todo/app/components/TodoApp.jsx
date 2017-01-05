a94bd7439c381e80bdbd70751261854c7684ae84
tsvetkovpro <285019@gmail.com>
2017-01-05 08:43:12 +0500

    creating the root component todoapp

---
+++ todo/app/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+	render: function () {
+		return (
+			<div>
+				todo.app.jsx
+			</div>
+		);
+	}
+});
+
+module.exports = TodoApp;
