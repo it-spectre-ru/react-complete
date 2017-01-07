e8019b791e3084b42fe12918aa879e2e3402bf84
tsvetkovpro <285019@gmail.com>
2017-01-07 21:59:04 +0500

    storing todos in local storage

--- todo/app/components/TodoApp.jsx
+++ todo/app/components/TodoApp.jsx
@@ -1,34 +1,22 @@
 var React = require ('react');
+var uuid = require ('node-uuid');
+
 var TodoList = require ('TodoList');
 var AddTodo = require ('AddTodo');
 var TodoSearch = require ('TodoSearch');
-var uuid = require ('node-uuid');
+var TodoAPI = require ('TodoAPI');
+
 
 var TodoApp = React.createClass ({
 	getInitialState: function () {
 		return {
 			showCompleted: false,
 			searchText: '',
-			todos: [
-				{
-					id: uuid (),
-					text: 'Walk the dog',
-					completed: false
-				}, {
-					id: uuid (),
-					text: 'Clean the yard',
-					completed: true
-				}, {
-					id: uuid (),
-					text: 'Leave mail on porch',
-					completed: false
-				}, {
-					id: uuid (),
-					text: 'Play video games',
-					completed: true
-				}
-			]
+			todos: TodoAPI.getTodos ()
 		};
+	},
+	componentDidUpdate: function () {
+		TodoAPI.setTodos (this.state.todos);
 	},
 	handleAddTodo: function (text) {
 		this.setState ({
@@ -44,14 +32,14 @@ var TodoApp = React.createClass ({
 	},
 	handleToggle: function (id) {
 		var updatedTodos = this.state.todos.map ((todo) => {
-			if(todo.id === id) {
+			if (todo.id === id) {
 				todo.completed = !todo.completed;
 			}
 
 			return todo;
 		});
 
-		this.setState({todos: updatedTodos});
+		this.setState ({todos: updatedTodos});
 	},
 	handleSearch: function (showCompleted, searchText) {
 		this.setState ({
