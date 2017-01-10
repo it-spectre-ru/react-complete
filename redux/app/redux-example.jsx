06ab12e7c7c730b8ef8c2b8e389c5f6ec09e4576
tsvetkovpro <285019@gmail.com>
2017-01-10 21:16:44 +0500

    pure functions

--- redux/app/redux-example.jsx
+++ redux/app/redux-example.jsx
@@ -1,3 +1,44 @@
 var redux = require('redux');
 
 console.log('start redux example');
+
+// pure function
+function add(a, b) {
+	return a + b;
+}
+
+
+var a = 3;
+function add(b) {
+	return a + b;
+}
+
+
+var result;
+function add(a, b) {
+	result = a + b;
+	return result;
+}
+
+function add(a, b) {
+	return a + b + new Date().getSeconds();
+}
+
+
+function changeProp(obj) {
+	return {
+		...obj,
+		name: 'jen'
+	};
+	//obj.name = 'jen';
+	//return obj;
+}
+
+var startingVaule = {
+	name: 'andrew',
+	age: 26
+}
+
+var res = changeProp(startingVaule);
+console.log(startingVaule);
+console.log(res);
