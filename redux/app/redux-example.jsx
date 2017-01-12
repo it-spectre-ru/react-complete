8a5bb985076148934bdfb03dda966bb72978df83
tsvetkovpro <285019@gmail.com>
2017-01-12 07:53:13 +0500

    redux actions, add to arrays

--- redux/app/redux-example.jsx
+++ redux/app/redux-example.jsx
@@ -2,7 +2,15 @@ var redux = require('redux');
 
 console.log('start redux example');
 
-var reducer = (state = {name: 'Anonymous'}, action) => {
+var stateDefault = {
+	name: 'anonymous',
+	hobbies: [],
+	noobies: []
+};
+
+var nextHobbyId = 1;
+var nextNoobyId = 1;
+var reducer = (state = stateDefault, action) => {
 	// state = state || {name: 'Anonymous'};
 
 	switch (action.type) {
@@ -10,6 +18,30 @@ var reducer = (state = {name: 'Anonymous'}, action) => {
 			return {
 				...state,
 				name: action.name
+			};
+		case 'ADD_HOBBY':
+			return {
+				...state,
+				hobbies: [
+					...state.hobbies,
+					{
+						id: nextHobbyId++,
+						hobby: action.hobby
+					}
+				]
+			};
+		case 'ADD_NOOBIES':
+			return {
+				...state,
+				noobies: [
+					...state.noobies,
+					{
+						id: nextNoobyId++,
+						noobby: action.noobby,
+						kind: action.kind,
+						title: action.title
+					}
+				]
 			};
 		default:
 			return state;
@@ -26,6 +58,8 @@ var unsubscribe = store.subscribe(() => {
 
 	console.log('name is: ', state.name);
 	document.getElementById('app').innerHTML = state.name;
+
+	console.log('New state: ', store.getState());
 });
 // unsubscribe();
 
@@ -36,6 +70,25 @@ console.log('current: ', currentState);
 store.dispatch({
 	type: 'CHANGE_NAME',
 	name: 'Piter'
+});
+
+store.dispatch({
+	type: 'ADD_HOBBY',
+	hobby: 'Running'
+});
+
+store.dispatch({
+	type: 'ADD_NOOBIES',
+	noobby: 'noobs_)',
+	kind: 'off',
+	title: 'Title noobies'
+});
+
+store.dispatch({
+	type: 'ADD_NOOBIES',
+	noobby: 'noobs_)2222222',
+	kind: 'off2222222222222',
+	title: 'Title noobies2222222222222222222'
 });
 
 store.dispatch({
