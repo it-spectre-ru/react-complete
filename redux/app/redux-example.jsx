cd4a9332cd32bb29b872c1b76af5a01c408739f9
tsvetkovpro <285019@gmail.com>
2017-01-13 20:51:07 +0500

    creating action generators

--- redux/app/redux-example.jsx
+++ redux/app/redux-example.jsx
@@ -1,72 +1,28 @@
 var redux = require('redux');
 
-console.log('start redux example');
+console.log('Starting redux example');
 
-var stateDefault = {
-	name: 'anonymous',
-	hobbies: [],
-	noobies: []
-};
-
-var nextHobbyId = 1;
-var nextNoobyId = 1;
-var oldReducer = (state = stateDefault, action) => {
-	// state = state || {name: 'Anonymous'};
-
-	switch (action.type) {
-		case 'CHANGE_NAME':
-			return {
-				...state,
-				name: action.name
-			};
-		case 'ADD_HOBBY':
-			return {
-				...state,
-				hobbies: [
-					...state.hobbies,
-					{
-						id: nextHobbyId++,
-						hobby: action.hobby
-					}
-				]
-			};
-		case 'REMOVE_HOBY':
-			return {
-				...state,
-				hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
-			};
-		case 'ADD_NOOBIES':
-			return {
-				...state,
-				noobies: [
-					...state.noobies,
-					{
-						id: nextNoobyId++,
-						noobby: action.noobby,
-						kind: action.kind,
-						title: action.title
-					}
-				]
-			};
-		case 'REMOVE_NOOBIES':
-			return {
-				...state,
-				noobies: state.noobies.filter((nobby) => nobby.kind !== action.kind)
-			};
-		default:
-			return state;
-	}
-};
-
+// Name reducer and action generators
+// -----------------
 var nameReducer = (state = 'Anonymous', action) => {
 	switch (action.type) {
 		case 'CHANGE_NAME':
 			return action.name;
 		default:
 			return state;
+	};
+};
+
+var changeName = (name) => {
+	return {
+		type: 'CHANGE_NAME',
+		name
 	}
 };
 
+// Hobbies reducer and action generators
+// -----------------
+var nextHobbyId = 1;
 var hobbiesReducer = (state = [], action) => {
 	switch (action.type) {
 		case 'ADD_HOBBY':
@@ -77,97 +33,95 @@ var hobbiesReducer = (state = [], action) => {
 					hobby: action.hobby
 				}
 			];
-		case 'REMOVE_HOBY':
-			return state.filter((nobby) => nobby.id !== action.id)
+		case 'REMOVE_HOBBY':
+			return state.filter((hobby) => hobby.id !== action.id);
 		default:
 			return state;
 	}
 };
 
-var noobiesReducer = (state = [], action) => {
+var addHobby = (hobby) => {
+	return {
+		type: 'ADD_HOBBY',
+		hobby
+	};
+};
+
+var removeHobby = (id) => {
+	return {
+		type: 'REMOVE_HOBBY',
+		id
+	};
+};
+
+// Movies reducer and action generators
+// -----------------
+var nextMovieId = 1;
+var moviesReducer = (state = [], action) => {
 	switch (action.type) {
-		case 'ADD_NOOBIES':
+		case 'ADD_MOVIE':
 			return [
 				...state,
 				{
-					id: nextNoobyId++,
-					noobby: action.noobby,
-					kind: action.kind,
-					title: action.title
+					id: nextMovieId++,
+					title: action.title,
+					genre: action.genre
 				}
-			];
-		case 'REMOVE_NOOBIES':
-			return state.filter((nobby) => nobby.kind !== action.kind)
+			]
+		case 'REMOVE_MOVIE':
+			return state.filter((movie) => movie.id !== action.id)
 		default:
 			return state;
 	}
+};
+
+var addMovie = (title, genre) => {
+	return {
+		type: 'ADD_MOVIE',
+		title,
+		genre
+	};
+};
+
+var removeMovie = (id) => {
+	return {
+		type: 'REMOVE_MOVIE',
+		id
+	};
 };
 
 var reducer = redux.combineReducers({
 	name: nameReducer,
 	hobbies: hobbiesReducer,
-	noobies: noobiesReducer
+	movies: moviesReducer
 });
 
 var store = redux.createStore(reducer, redux.compose(
 	window.devToolsExtension ? window.devToolsExtension() : f => f
 ));
 
-// subscribe to changes
+// Subscribe to changes
 var unsubscribe = store.subscribe(() => {
 	var state = store.getState();
 
-	console.log('name is: ', state.name);
+	console.log('Name is', state.name);
 	document.getElementById('app').innerHTML = state.name;
 
-	console.log('New state: ', store.getState());
+	console.log('New state', store.getState());
 });
 // unsubscribe();
 
 var currentState = store.getState();
-console.log('current: ', currentState);
+console.log('currentState', currentState);
 
+store.dispatch(changeName('Andrew'));
 
-store.dispatch({
-	type: 'CHANGE_NAME',
-	name: 'Piter'
-});
+store.dispatch(addHobby('Running'));
+store.dispatch(addHobby('Walking'));
+store.dispatch(removeHobby(2));
 
-store.dispatch({
-	type: 'ADD_HOBBY',
-	hobby: 'Running'
-});
+store.dispatch(changeName('Emily'));
 
-store.dispatch({
-	type: 'ADD_HOBBY',
-	hobby: 'Walking'
-});
-
-store.dispatch({
-	type: 'REMOVE_HOBY',
-	id: 2
-});
-
-store.dispatch({
-	type: 'ADD_NOOBIES',
-	noobby: 'noobs_)',
-	kind: 'off',
-	title: 'Title noobies'
-});
-
-store.dispatch({
-	type: 'ADD_NOOBIES',
-	noobby: 'noobs_)2222222',
-	kind: 'off2222222222222',
-	title: 'Title noobies2222222222222222222'
-});
-
-store.dispatch({
-	type: 'REMOVE_NOOBIES',
-	kind: 'off'
-});
-
-store.dispatch({
-	type: 'CHANGE_NAME',
-	name: 'emily'
-});
+store.dispatch(addMovie('Mad Max', 'Action'));
+store.dispatch(addMovie('Start Wars', 'Action'));
+store.dispatch(removeMovie(1));
