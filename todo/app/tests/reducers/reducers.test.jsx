277e31f3291052a85f5c782fbff8a81e9393c6a8
tsvetkovpro <285019@gmail.com>
2017-01-19 20:35:13 +0500

    testing async actions

--- todo/app/tests/reducers/reducers.test.jsx
+++ todo/app/tests/reducers/reducers.test.jsx
@@ -30,12 +30,17 @@ describe('Reducers', () => {
 		it('should add new todo', () => {
 			var action = {
 				type: 'ADD_TODO',
-				text: 'to be there'
+				todo: {
+					id: '123',
+					text: 'some-text',
+					completed: false,
+					createdAt: 12345665
+				}
 			};
 			var res = reducers.todosReducer(df([]), df(action));
 
 			expect(res.length).toEqual(1);
-			expect(res[0].text).toEqual(action.text);
+			expect(res[0]).toEqual(action.todo);
 		});
 
 		it('should toggle todo', () => {
